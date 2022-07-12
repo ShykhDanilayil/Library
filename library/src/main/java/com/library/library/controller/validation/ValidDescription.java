5cdc31a74a8bcdf0c59b169de4dfed1c0c5c1146
danyilshykh <danyil.shykh@gmail.com>
2022-07-12 10:17:43 +0300

    added validation

---
+++ library/src/main/java/com/library/library/controller/validation/ValidDescription.java
@@ -0,0 +1,23 @@
+package com.library.library.controller.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = DescriptionValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidDescription {
+
+    String message() default "Invalid book description";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
