b14d1a6b2bff0b7889b92d9ad6025819380a219e
danyilshykh <danyil.shykh@gmail.com>
2022-07-14 10:40:05 +0300

    update test

--- library/src/test/java/com/library/library/LibraryApplicationTests.java
+++ library/src/test/java/com/library/library/LibraryApplicationTests.java
@@ -1,20 +1,27 @@
 package com.library.library;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryApplicationTests {
 
-//	@LocalServerPort
-//	private int port;
-//
-//	@Autowired
-//	private TestRestTemplate restTemplate;
-//
-//	@Test
-//	void contextLoads() {
-//		HttpStatus statusCode = restTemplate.getForEntity("http://localhost:" + port + "/actuator/health", String.class).getStatusCode();
-//		assertEquals(statusCode, HttpStatus.OK);
-//	}
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void contextLoads() {
+		HttpStatus statusCode = restTemplate.getForEntity("http://localhost:" + port + "/actuator/health", String.class).getStatusCode();
+		assertEquals(statusCode, HttpStatus.OK);
+	}
 
 }
