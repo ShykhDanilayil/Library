f7c3c0000930f046dfe3470c3b9aac729bbafe78
danyilshykh <danyil.shykh@gmail.com>
2022-08-01 13:08:34 +0300

    update swagger

--- library/src/main/java/com/library/library/config/SwaggerConfig.java
+++ library/src/main/java/com/library/library/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,48 +22,66 @@ import java.util.List;
 @EnableSwagger2
 public class SwaggerConfig {
 
-  @Bean
-  public Docket libraryApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .groupName("library")
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
-        .paths(PathSelectors.ant("/library/**"))
-        .build();
-  }
-  @Bean
-  public Docket userApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .groupName("user")
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
-        .paths(PathSelectors.ant("/user/**"))
-        .build();
-  }
-  @Bean
-  public Docket authorApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .groupName("author")
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
-        .paths(PathSelectors.ant("/author/**"))
-        .build();
-  }
-  @Bean
-  public Docket bookApi() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .groupName("book")
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
-        .paths(PathSelectors.ant("/book/**"))
-        .build();
-  }
+    @Bean
+    public Docket libraryApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("library")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
+                .paths(PathSelectors.ant("/libraries/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
 
-  @Bean
-  public LinkDiscoverers discoverers() {
-    List<LinkDiscoverer> plugins = new ArrayList<>();
-    plugins.add(new CollectionJsonLinkDiscoverer());
-    return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-  }
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("user")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
+                .paths(PathSelectors.ant("/users/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public Docket authorApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("author")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
+                .paths(PathSelectors.ant("/authors/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public Docket bookApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("book")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.library.library.controller"))
+                .paths(PathSelectors.ant("/books/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Profile Api")
+                .contact(new Contact(
+                        "",
+                        "",
+                        "danyil.shykh@gmail.com"
+                ))
+                .build();
+    }
 
 }
