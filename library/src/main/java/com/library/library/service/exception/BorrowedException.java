3fa087433876dcd87f343ded11b305e7e9770cff
danyilshykh <danyil.shykh@gmail.com>
2022-08-01 13:11:48 +0300

    updated exception

---
+++ library/src/main/java/com/library/library/service/exception/BorrowedException.java
@@ -0,0 +1,8 @@
+package com.library.library.service.exception;
+
+public class BorrowedException extends RuntimeException {
+
+    public BorrowedException(String message) {
+        super(message);
+    }
+}
