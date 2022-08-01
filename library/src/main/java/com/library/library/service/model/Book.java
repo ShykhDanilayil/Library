69dc0fa8639b467abafc44457bb4f5504dc0d771
danyilshykh <danyil.shykh@gmail.com>
2022-08-01 13:12:21 +0300

    updated model

--- library/src/main/java/com/library/library/service/model/Book.java
+++ library/src/main/java/com/library/library/service/model/Book.java
@@ -1,34 +1,41 @@
 package com.library.library.service.model;
 
+import com.library.library.controller.dto.BookStatus;
 import com.library.library.controller.dto.Genre;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Data
 @Entity
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = "library")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Long authorId;
     private String title;
     private String description;
     private int pages;
     private int publicationYear;
-    @Enumerated(EnumType.ORDINAL)
+    @Enumerated(EnumType.STRING)
     private Genre genre;
-    @ManyToMany(mappedBy = "books")
-    private List<Library> libraries = new ArrayList<>();
-
+    @Enumerated(EnumType.STRING)
+    private BookStatus status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "library_id")
+    private Library library;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
---
+++ library/src/main/java/com/library/library/service/model/Borrowed.java
@@ -0,0 +1,41 @@
+package com.library.library.service.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Borrowed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Timestamp dated;
+    private Date dueDate;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
+    private Book book;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Library library;
+
+    public Borrowed(Date dueDate, Book book, User user, Library library) {
+        this.dated = new Timestamp(System.currentTimeMillis());
+        this.dueDate = dueDate;
+        this.book = book;
+        this.user = user;
+        this.library = library;
+    }
+}
