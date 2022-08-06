99a591b910152c58ceef9cc03b6b113634148172
danyilshykh <danyil.shykh@gmail.com>
2022-08-06 13:35:13 +0300

    updated user repository

--- library/src/main/java/com/library/library/service/repository/UserRepository.java
+++ library/src/main/java/com/library/library/service/repository/UserRepository.java
@@ -13,6 +13,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
 
     boolean existsUserByEmail(String email);
-
-    boolean existsUserByUserName(String name);
 }
